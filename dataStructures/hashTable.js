e722c6dcdf28ea59bfc4a05d4e1e3a62a81f03ff
cmir22 <isc.cruzibarra@gmail.com>
2023-09-26 20:57:14 -0700

    Learning

---
+++ dataStructures/hashTable.js
@@ -0,0 +1,54 @@
+//
+// Hash table
+//
+
+class hashTable {
+  constructor(size) {
+    this.size = size;
+    this.data = new Array(size);
+  }
+
+  _hash(key = "") {
+    let hash = 0;
+
+    for (let i = 0; i < key.length; i++) {
+      hash += (key.charCodeAt(i) * i) % this.size;
+    }
+
+    return hash;
+  }
+
+  set(key, value) {
+    const address = this._hash(key);
+    if (!this.data[address]) {
+      this.data[address] = [];
+    }
+
+    this.data[address].push([key, value]);
+    return this.data;
+  }
+
+  get(key) {
+    const address = this._hash(key);
+    const currentBucket = this.data[address];
+
+    if (currentBucket) {
+      for (let i = 0; i < currentBucket.length; i++) {
+        if (currentBucket[i][0] === key) {
+          return currentBucket[i][0];
+        }
+      }
+    }
+
+    return undefined;
+  }
+}
+
+const myHashTable = new hashTable(50);
+
+myHashTable.set("John Doe", 28);
+myHashTable.set("Jane Smith", 32);
+
+// console.log(myHashTable.get("John Doe")); // Output: 28
+// console.log(myHashTable.get("Jane Smith")); // Output: 32
+// console.log(myHashTable.get("Bob Johnson")); // Output: undefined
